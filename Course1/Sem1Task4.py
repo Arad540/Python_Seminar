ad4a6a09eda4f7052fa84581976e6f0e35b4010e
Арефи Арад <a.arefi@internal.avilex.ru>
2023-05-19 13:08:14 +0300

    add Sem1Task4

---
+++ Course1/Sem1Task4.py
@@ -0,0 +1,19 @@
+n = int(input('Put the number of cranes: '))
+
+
+
+if n % 6 == 0:
+     d1 = n // 6
+     d2 = (n //6)*4
+     d3 = n // 6
+     print ('Kate cranes: ', d2, 'Petya cranes: ', d1, 'Serozha cranes: ', d3 )
+     
+else:
+    print ('I cant handle this number')
+    
+    
+    
+             
+   
+
+  
