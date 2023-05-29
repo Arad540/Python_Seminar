7da4b5925836217b6d8d11e021bdd7acc9267aa5
Arad Arefi <a.arefi@mail.ru>
2023-05-29 21:39:22 +0300

    add task20

--- Course1/Sem#Task20.py
+++ Course1/Sem#Task20.py
@@ -1,11 +1,14 @@
-# data = {'1': ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'], 
-#         '2':['D', 'G' ], '3':['B', 'C', 'M', 'P'],
-#         '4':['F', 'H', 'V', 'W', 'Y'], '4': ['K'], '5' :['K'], '8' : ['J', 'X'], '6': ['Q', 'Z']}
-
-# NewWord = for i in input ('put yor word ').split()
-# print (NewWord)
-
-input_string = input('Enter elements of a list separated by space ')
-user_list = input_string
-# print list
-print('list: ', user_list)
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JZ', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = input().upper()
+count = 0
+for i in word:
+    if i in 'qwertyuiopasdfghjklzxcvbnm'.upper():
+        for j in points_en:
+            if i in points_en[j]:
+                count += j
+    else:
+        for j in points_ru:
+            if i in points_ru[j]:
+                count += j
+print(count)
